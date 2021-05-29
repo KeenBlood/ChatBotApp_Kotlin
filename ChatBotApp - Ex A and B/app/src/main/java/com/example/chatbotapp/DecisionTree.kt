a19ff3f8b0042fbe009f13994bd03dbdc1546f47
KeenBlood <keenblood.contact@gmail.com>
2021-05-29 22:23:22 +0100

    v1.01

--- ChatBotApp - Ex A and B/app/src/main/java/com/example/chatbotapp/DecisionTree.kt
+++ ChatBotApp - Ex A and B/app/src/main/java/com/example/chatbotapp/DecisionTree.kt
@@ -4,83 +4,212 @@ object DecisionTree {
 
     fun response (message_receiver: String): String
     {
-        val random = (0..1).random()
+        val random = (0..4).random() //gerar um número aleatorio de 0 a 4
 
-        val message = message_receiver.toLowerCase()
+        val other_random = (0..1).random() //gerar um número aleatorio de 0 a 1
+
+        val message = message_receiver.toLowerCase() //colocar aqui a mensgem que recebe
+
+
 
         return when {
-            message.contains("sistca") && message.contains("name") && message.contains("complete")-> {
+
+            //Goodbye
+
+            message.contains("goodbye") -> {
+
                 when (random) {
-                    0 -> "The name is Adevenced Computing systems, with means Sistemas computacionais avançados in Portuguese"
-                    1 -> "In Portuguese is Sistemas computacionais avançados"
+
+                    1 -> "Seen you soon, I hope!"
+
+                    2 -> "Goodbye..."
+
+                    3 -> "Byeee."
+
+                    4 -> "Bye, go st"
 
                     else -> "error"
-                }   }
-            message.contains("sistca") && message.contains("extensive") -> {
-                when (random) {
-                    0 -> "The name is Sistemas computacionais avançados"
-                    1 -> "Sistemas computacionais avançados"
 
-                    else -> "error"
                 }
+
             }
-            message.contains("sistca") && message.contains("ects") -> {
+
+            //Hello
+
+            message.contains("hello") -> {
+
+
+
                 when (random) {
-                    0 -> "6 ECTS"
-                    1 -> "Is the result of 61-55!"
+
+                    0 -> "Hi dear human."
+
+                    1 -> "Hello!"
+
+                    2 -> "Hi, hope you are okay."
+
+                    3 -> "Hi, I'm a bot."
+
+                    4 -> "Good to see you!"
 
                     else -> "error"
+
                 }
+
             }
-            message.contains("sistca") && message.contains("hours") && message.contains("theoretical") -> {
-                when (random) {
-                    0 -> "2 weekly"
-                    1 -> "It's just 2 hours weekly!"
+
+            //anchor
+
+            message.contains("anchor") && message.contains("what") && message.contains("for")  -> {
+
+                when (other_random) {
+
+                    0 -> "Is like anchor a ship, so it doesn't move to much. There you ancor the layout, so it the placement is similiar even when the system has a different resolution."
+
+                    1 -> "It's explained in the report, read again, c’mon!"
 
                     else -> "error"
+
                 }
+
             }
-            message.contains("sistca") && message.contains("hours") && message.contains("practical") -> {
-                when (random) {
-                    0 -> "2 weekly"
-                    1 -> "It's just 2 hours weekly!"
+
+            // how to built
+
+            message.contains("build") && message.contains("how")  -> {
+
+                when (other_random) {
+
+                    0 -> "You should click in the green hammer."
+
+                    1 -> "Did you already try to click in the green hammer?"
 
                     else -> "error"
+
                 }
+
             }
-            message.contains("sistca") && message.contains("exam") && message.contains("%") -> {
-                when (random) {
-                    0 -> "60% of the final grade"
-                    1 -> "60% of the final grade. Keep studying!"
+
+            //why do i built
+
+            message.contains("build") && message.contains("why")  -> {
+
+                when (other_random) {
+
+                    0 -> "To compile!"
+
+                    1 -> "Did you learn that in the 1st year of university? okay, okay, I can tell you. Is to compile."
 
                     else -> "error"
+
                 }
+
             }
-            message.contains("sistca") && message.contains("exam") && message.contains("percentage") -> {
-                when (random) {
-                    0 -> "60% of the final grade"
-                    1 -> "60% of the final grade. Keep studying!"
+
+            // how to run
+
+            message.contains("run") && message.contains("how")   -> {
+
+                when (other_random) {
+
+                    0 -> "You should click in the green arrow."
+
+                    1 -> "Did you already try to click in the green arrow?"
 
                     else -> "error"
+
                 }
+
             }
-            message.contains("sistca") && message.contains("exam") && message.contains("minimum") -> {
-                when (random) {
-                    0 -> "8.0 values but try to have more than that!"
-                    1 -> "8.0 values."
+
+            // taking much time to run
+
+            message.contains("run") && message.contains("taking")  && message.contains("time")    -> {
+
+                when (other_random) {
+
+                    0 -> "Is your first time running the program? If so, calm down, is normal. If has been 5 minutes or more, cancel that and see in the report if you code is in the parameters."
+
+                    1 -> "Breath in, breath out, if everything is fine it will wourk out! Just kidding, wait a bit first. If your problem continues, try to see if your emulator is okay, if not, delete it and create a new one."
 
                     else -> "error"
+
                 }
+
             }
+
+            // error
+
+            message.contains("not") && message.contains("working")   -> {
+
+                when (other_random) {
+
+                    0 -> "Check if everything is equal or similar, then try again please"
+
+                    1 -> "Did you put the imports that are in the report?"
+
+                    else -> "error"
+
+                }
+
+            }
+
+            //separeted text
+
+            message.contains("separate") && message.contains("text") -> {
+
+                when (other_random) {
+
+                    0 -> "You wrote Separate text separately."
+
+                    1 -> "You wrote separate and text in the same sentence and in the same sentence."
+
+                    else -> "error"
+
+                }
+
+            }
+
+
+
+            //palavra a vermelho
+
+            message.contains("red") && message.contains("word") -> {
+
+                when (other_random) {
+
+                    0 -> "A red word can appear because a lot of reasons, put you mouse there, click it. a light bulb will appear in the beginning of that line, click it and that will tell you the problem."
+
+                    1 -> "Make sure you are writing that word correctly. If it is correct, a ligh bulb will apper in the beginning of that line, click it and that will tell you the problem."
+
+                    else -> "error"
+
+                }
+
+            }
+
+
 
             else -> {
+
                 when (random) {
-                    0 -> "If it'ss about SISTCA, see in the portal or in moodle or even, ask to a teacher"
+
+                    0 -> "Can you ask something else?"
+
                     1 -> "I don't know that, ask me other thing please :c"
 
+                    2 -> "I didn't understand you..."
+
+                    3 -> "Ask to someone else, I don't know."
+
+                    4->   "I'm going to tell you a secret: I don't know that."
+
                     else -> "error"
+
                 }
+
             }
+
         }
     }
 
